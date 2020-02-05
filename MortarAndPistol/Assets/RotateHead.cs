2ca7a90a5e8f8e3ba792a8479f8abb62f3aaa335
joelyou20 <joelcribby@live.ca>
2020-02-05 00:43:26 -0800

    chore: add idle animation

---
+++ MortarAndPistol/Assets/RotateHead.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHead : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 lookDirection;
+        lookDirection = Input.mousePosition;
+        lookDirection.z = 0.0f;
+        lookDirection = Camera.main.ScreenToWorldPoint(lookDirection);
+        lookDirection = lookDirection - transform.position;
+
+        transform.rotation = new Quaternion(lookDirection.x, lookDirection.y, 0, 0);
+    }
+}
